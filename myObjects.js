915141f1e7eca41fbcc03c593c24fb33359fdca1
Kaushik Reddy <reddykaushik18@gmail.com>
2019-05-30 19:28:52 +0530

    Added object in JS-files

---
+++ myObjects.js
@@ -0,0 +1,40 @@
+var user = {
+	name: "Kaushik",
+	age: 18,
+	IsMarried : false,
+	sports: "Cricket",
+	fav_song: function{
+    console.log("We will..we will...rock youuu!!!");
+	}
+}
+
+list_of_Objs = [
+{
+	name: "ravi",
+	age:21,
+	IsMarried: true,
+	sports: "Soccer",
+	fav_song: function{
+    console.log("lovvee meee like you do...!!!");
+	}
+},
+{
+	name:"anand",
+	age:30,
+	IsMarried: true,
+	sports:"hockey",
+	fav_song: function
+	{
+		console.log("Burnn it downn!!");
+	}
+},
+{
+	name:"sachin",
+	age:45,
+	IsMarried:false,
+	sports: "Table-tennis",
+	fav_song:function{
+		console.log("What've I donne!!");
+	}
+}
+]
